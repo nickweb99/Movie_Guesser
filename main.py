7da80cd12a3f3e9e8e7df6b38ad0cdafbc563643
Nicholas Santarelli <nickweb99@outlook.com>
2025-09-17 21:29:43 -0400

    Set up API access

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 
+import configparser
+import requests
+
+config = configparser.ConfigParser()
+
+try:
+    config.read('config.ini')
+except configparser.Error as e:
+    print(f"Error reading config file: {e}")
+
+api_key = config['API_SETTINGS']['api_key']
+
+url = "https://api.themoviedb.org/3/authentication"
+
+headers = {
+    "accept": "application/json",
+    "Authorization": api_key
+}
+
 def main():
     print("Welcome to the Movie Guesser game!")
+
+    response = requests.get(url, headers=headers)
+
+    print(response.text)
 
 main()
