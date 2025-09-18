353e2bf7776574a4023c41c8d55f4c7637c2f7a4
Nicholas Santarelli <nickweb99@outlook.com>
2025-09-17 21:59:14 -0400

    Got a list of movies from the api

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 import configparser
 import requests
+import random
+import json
 
 config = configparser.ConfigParser()
 
@@ -11,8 +13,6 @@ except configparser.Error as e:
 
 api_key = config['API_SETTINGS']['api_key']
 
-url = "https://api.themoviedb.org/3/authentication"
-
 headers = {
     "accept": "application/json",
     "Authorization": api_key
@@ -20,9 +20,17 @@ headers = {
 
 def main():
     print("Welcome to the Movie Guesser game!")
+    # #Api test response
+    # API_Test = requests.get("https://api.themoviedb.org/3/authentication", headers=headers)
+    # print(API_Test.text)
 
-    response = requests.get(url, headers=headers)
+    rand = random.randint(1, 10)#Replace with upperbound on initial data pull
 
-    print(response.text)
+    main_data_response = requests.get("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_original_language=en", headers=headers)
+    main_data = main_data_response.json()
+    movies = main_data["results"]
+    for movie in movies:
+        print(movie["original_title"])
+        print("------------")
 
 main()
