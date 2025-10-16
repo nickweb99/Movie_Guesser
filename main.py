269447fac060bccca047802519c6a2cc0a51f5ae
Nicholas Santarelli <nickweb99@outlook.com>
2025-10-15 20:46:44 -0400

    Added more clue possiblities and made it so answer does not have to be 100% correct

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 import random
 import json
 import msvcrt
+from rapidfuzz import fuzz
 
 config = configparser.ConfigParser()
 
@@ -37,15 +38,19 @@ def main():
             return
 
     Selection = GetRandomMovie()
+    Win = False
     print(Selection)#["original_title"])
     for i in range(1,6):
+        if Win == True:
+            break
         print(f"Here is your clue number {i}:")
-        GenerateClue(i)
+        GenerateClue(i, Selection)
     
         while True:
             guess = input("Name the movie:")
-            if guess == Selection["original_title"]:
-                print("You Win!")
+            #if guess == Selection["original_title"]:
+            if guess != "" and CloseGuess(guess, Selection["original_title"]):
+                Win = True
                 break
             elif guess == "":
                 clear_last_line()
@@ -53,24 +58,47 @@ def main():
             else:
                 print("Incorrect")
             break
+    
+    if Win == True:
+        print("You Win!")
+    else:
+        print(f"The movie was {Selection["original_title"]}")
+        print("You Lose.")
 
-def GenerateClue(guess):
+def CloseGuess(Input, Model):
+    score = fuzz.ratio(Input.lower(), Model.lower())
+    if score > 80:
+        return True
+    else:
+        return False
+
+def GenerateClue(guess, Selection):
+    print(guess)
     match guess:
         case 1:
             return
         case 2:
-            return
+            GenreResponse = requests.get("https://api.themoviedb.org/3/genre/movie/list?language=en", headers=headers)
+            GenreData = GenreResponse.json()
+            Genres = GenreData["genres"]
+            OurGenres = Selection["genre_ids"]
+            print("Here are this movies Genres:")
+            for OurGenre in OurGenres:
+                for Genre in Genres:
+                    if Genre["id"] == OurGenre:
+                        print(Genre['name'])
+                    
         case 3:
-            return
+            print(f"This movie was released on {Selection["release_date"]}")
         case 4:
+            print(f"This movie starts with the letter: {Selection["original_title"][0]}")
             return
         case 5:
+            print("Here is this movies synopsis:")
+            print(Selection["overview"])
             return
         
         #Clue options
-        #Display overview
-        #genre_ids
-        #release_date
         #Ascii Version of image? 
         #Cast? not sure if I can get this from the api
 
@@ -79,6 +107,7 @@ def GetRandomMovie():
     page = random.randint(1, 99)
     main_data_response = requests.get("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=99&sort_by=popularity.desc&with_original_language=en&vote_count.gte=1000", headers=headers)
     main_data = main_data_response.json()
+    #print(main_data)
     movies = main_data["results"]
     rand = random.randint(1, len(movies))
     # for movie in movies:
@@ -90,3 +119,7 @@ def GetRandomMovie():
 
 
 main()
+
+#Updates: 
+#Fix randomness, does not seem random enough
+#Make it so that close guesses work if they are close enough
